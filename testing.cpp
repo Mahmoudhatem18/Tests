5021f93b1bdea9ba53a4dbd7c229254ba92ebb13
Mahmoud Hatem <mahmoudddhatem18@gmail.com>
2025-11-15 01:23:42 +0200

    Changing Variable name

---
+++ testing.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    cout << a + b;
+
+    return 0;
+}
